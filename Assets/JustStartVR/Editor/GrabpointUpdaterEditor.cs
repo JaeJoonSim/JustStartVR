8485429422d210424715b4791ad69423bfbab0c0
JaeJoonSim <tlawowns78@gmail.com>
2022-05-03 13:12:50 +0900

    Rebuild VR System
    vr system 재구축
    이전 프로젝트 : https://github.com/JaeJoonSim/Just_Start

---
+++ Assets/JustStartVR/Editor/GrabpointUpdaterEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace JustStartVR {
+
+    [CustomEditor(typeof(GrabpointUpdater))]
+    [CanEditMultipleObjects]
+    public class GrabpointUpdaterEditor : Editor {
+
+        GrabpointUpdater updater;
+
+        void OnEnable() {
+           
+        }
+
+        public override void OnInspectorGUI() {
+
+            updater = (GrabpointUpdater)target;
+
+            EditorGUILayout.HelpBox("This component will help you autmatically update your grab points, as the default hand model positions have changed since VRIF v1.7. Click the 'Updated Pose' button to update the grab points in the editor. You can preview the changes in play mode before applying the changes in the editor.", MessageType.Info);
+
+            if (GUILayout.Button("Update Pose")) {
+                updater.ApplyGrabPointUpdate();
+            }
+
+            EditorGUILayout.Separator();
+
+            serializedObject.ApplyModifiedProperties();
+
+            base.OnInspectorGUI();
+        }
+    }
+}
+
+
---
+++ Assets/JustStartVR/Scripts/Helpers/UITrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this to trigger buttons or UI elements in addition to Grabber objects
+/// </summary>
+namespace JustStartVR {
+    public class UITrigger : MonoBehaviour {
+       
+    }
+}
+
