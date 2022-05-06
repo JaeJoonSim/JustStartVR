9735cee1afc9adb253dae100bc242b9e1e6cbebc
JaeJoonSim <tlawowns78@gmail.com>
2022-05-07 06:54:31 +0900

    Fixed Rigidbody

---
+++ Assets/JustStartVR Framework/Scripts/Components/FollowRigidbody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JustStartVR {
+    public class FollowRigidbody : MonoBehaviour {
+
+        public Transform FollowTransform;
+        Rigidbody rigid;
+
+        void Start() {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate() {
+            rigid.MovePosition(FollowTransform.transform.position);
+        }
+    }
+}
+
---
+++ Assets/JustStartVR Framework/Scripts/Core/GrabbableChild.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JustStartVR {
+    public class GrabbableChild : MonoBehaviour {
+        [Tooltip("The Parent Grabbable Object to be grabbed.")]
+        public Grabbable ParentGrabbable;
+    }
+}
