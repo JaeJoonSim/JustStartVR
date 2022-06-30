406e150fa6a9d7489ef59771d4678fa5e4f79e07
Mong_Gwak_du <97100624+GwakDuMong@users.noreply.github.com>
2022-06-30 14:53:53 +0900

    Revert "Merge branch 'master' into LDH"
    This reverts commit dbf67c40421ae7c7358e20c76fc99813160d7be9, reversing
    changes made to 4f80ffdfd66ebc51f77044046e9a2b3a75a0e257.

---
+++ Assets/Scripts/MapScript/MapInstance.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapInstance : MonoBehaviour
+{
+    public static MapInstance Instance;
+
+    public class Tile
+    {
+        public int x;
+        public int z;
+        public bool isRoom;
+        public Tile(int _x, int _z, bool room)
+        {
+            x = _x;
+            z = _z;
+            isRoom = room;
+        }
+    }
+
+    public List<Tile> m_TileList = new List<Tile>();
+    public const int m_TileSize = 2;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+}
