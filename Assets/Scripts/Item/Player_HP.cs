0ab49970d1c4991d23a2bf2e75c8171cfb4f257b
MyProject-JS <whoisjsimjs@gmail.com>
2022-07-01 21:22:11 +0900

    HP_UI

---
+++ Assets/Scripts/Item/Player_HP.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+    public class Player_HP : MonoBehaviour
+    {
+        public int HP;
+
+        public Image HP_Bar;
+
+
+        void Start()
+        {
+
+        }
+
+        
+
+    }
+
