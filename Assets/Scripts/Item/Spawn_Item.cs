f052265508d34fefdfa03e015f600a9d8b4c4810
JaeJoonSim <tlawowns78@gmail.com>
2022-07-01 22:50:06 +0900

    HP, 플래시
    Co-Authored-By: MyProject-JS <97032165+MyProject-JS@users.noreply.github.com>

---
+++ Assets/Scripts/Item/Spawn_Item.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Item : MonoBehaviour
+{
+    GameObject Item;
+
+    Transform[] Spawn_Point;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        
+    }
+}
