4055faaee7c16cde8dd4c9b5618f86ee7e74e681
MyProject-JS <whoisjsimjs@gmail.com>
2022-07-20 21:17:44 +0900

    시나라오 메모

--- Assets/Scripts/FollowRotation.cs
+++ Assets/Scripts/FollowRotation.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class FollowRotation : MonoBehaviour
 {
+    public bool stay = true;
     public GameObject FollowOBJ;
 
+    private void Start()
+    {
+        if(!stay)
+            transform.rotation = FollowOBJ.transform.rotation;
+    }
     private void Update()
     {
+        if(stay)
         transform.rotation = FollowOBJ.transform.rotation;
     }
 }
