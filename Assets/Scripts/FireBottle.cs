a292cc77d7af64b886cc9637f64aed4eb3fcc6a4
JaeJoonSim <tlawowns78@gmail.com>
2022-07-27 22:10:46 +0900

    화염병 이펙트추가 + 버그픽스

--- Assets/Scripts/FireBottle.cs
+++ Assets/Scripts/FireBottle.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 public class FireBottle : MonoBehaviour
 {
     [SerializeField]GameObject m_FireEffect;
+    [SerializeField] GameObject fireReadyEffect;
 
     public bool isHand { get; set; }
 
     private void Start()
     {
         isHand = false;
+        fireReadyEffect.SetActive(false);
+    }
+
+    private void Update()
+    {
+        fireReadyEffect.SetActive(isHand);
+        if (isHand)
+            gameObject.name = "FireBottle";
     }
 
     private void OnCollisionEnter(Collision collision)
