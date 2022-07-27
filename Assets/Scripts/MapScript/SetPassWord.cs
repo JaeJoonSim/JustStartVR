0298be565440f1b586d3cd7c6c827289f88cd9d4
staronthepark <asdffg6934@naver.com>
2022-07-28 01:25:35 +0900

    플레이어 타블렛 패스워드 표시

--- Assets/Scripts/MapScript/SetPassWord.cs
+++ Assets/Scripts/MapScript/SetPassWord.cs
@@ -27,7 +27,7 @@ public class SetPassWord : MonoBehaviour
 
     void Start()
     {
-        
+        playertablet = GameObject.Find("Player Tablet").GetComponent<PlayerTablet>();
         manager = GameObject.Find("Elevator").GetComponent<PassWordManager>();
         floor = (int)this.transform.position.y / 20;
         password = manager.number[manager.count];
