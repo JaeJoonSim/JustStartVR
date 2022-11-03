436a91eaea042d3351b487caccb11542331e7917
Mong_Gwak_du <97100624+GwakDuMong@users.noreply.github.com>
2022-11-04 00:23:49 +0900

    L_zombie add

--- Assets/Scripts/Enemy/GetHeadTransformNomal.cs
+++ Assets/Scripts/Enemy/GetHeadTransformNomal.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetHeadTransform : MonoBehaviour
+public class GetHeadTransformNomal : MonoBehaviour
 {
     public Transform head;
     private void Update()
     {
-        transform.eulerAngles = new Vector3(0f, head.eulerAngles.y+90f, 0f);
+        transform.eulerAngles = new Vector3(0f, head.eulerAngles.y, 0f);
         //head.rotation; //Quaternion.Euler(0, 90, 0) *
         //transform.eulerAngles = new Vector3(0f, head.localRotation.y, 0f);
         //Debug.Log(head.localRotation.y);
