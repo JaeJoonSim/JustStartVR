81a8d92c901772075bb9caeacd9ac5660cf38a3b
staronthepark <asdffg6934@naver.com>
2022-11-04 22:25:34 +0900

    .

--- Assets/Scenes/Scripts/SoundManager/SoundManager.cs
+++ Assets/Scenes/Scripts/SoundManager/SoundManager.cs
@@ -40,7 +40,7 @@ public class SoundManager : MonoBehaviour
         zombieIdle,
 
         tongue, tongueGrap, playerHit, playerHit2, BossFoot, gameOver, CrackGlass,
-        swing, neon
+        swing, neon, shutter
 
     }
 
